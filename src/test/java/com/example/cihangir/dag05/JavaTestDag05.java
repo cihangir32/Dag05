ef1795117a0d634aadfdb44ef4a7a9c18e687c5d
cihangir32 <cihangirfb1@gmail.com>
2020-09-15 13:47:37 +0200

    Initiele commit in nieuwe branch

--- src/test/java/com/example/cihangir/dag05/JavaTestDag05.java
+++ src/test/java/com/example/cihangir/dag05/JavaTestDag05.java
@@ -6,24 +6,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class JavaTestDag05 {
-    @Test // Annotatie
-    void JavaTest(){
+    @Test
+        // Annotatie
+    void JavaTest() {
 
         System.out.println("Hallo Java!");
     }
-    @Test // Annotatie
-    void JavaTest1(){
+
+    @Test
+        // Annotatie
+    void JavaTest1() {
         double doubleVar1 = 1.033;
         double doubleVar2 = 0.033;
         double difference = doubleVar1 - doubleVar2;
         boolean isGelijk = difference == 1.0;
         Offset<Double> offset = Offset.offset(1.0E-2);
-        assertThat(difference).isEqualTo(1.0,offset);
+        assertThat(difference).isEqualTo(1.0, offset);
 
     }
 
-    @Test // Annotatie
-    void JavaTest2(){
+    @Test
+        // Annotatie
+    void JavaTest2() {
         double doubleVar1 = 1.033;
         double doubleVar2 = 0.033;
         double difference = doubleVar1 - doubleVar2;
@@ -31,39 +35,41 @@ public class JavaTestDag05 {
         assertThat(difference).isEqualTo(1.0);
     }
 
-    @Test // Annotatie
-    void JavaTest3(){
+    @Test
+        // Annotatie
+    void JavaTest3() {
         byte b1 = 1;
         char char1 = 'a';
         char char2 = 'b';
         System.out.println("String test " + char1 + char2);
     }
 
-    @Test // Annotatie
+    @Test
+        // Annotatie
     void Tafels() {
         int num = 5;
-        for(int i = 1; i <= 10; ++i)
-        {
+        for (int i = 1; i <= 10; ++i) {
             System.out.printf("%d * %d = %d \n", num, i, num * i);
         }
         System.out.println(" ");
 
         int num1 = 6;
-        for(int i = 1; i <= 10; ++i)
-        {
+        for (int i = 1; i <= 10; ++i) {
             System.out.printf("%d * %d = %d \n", num1, i, num1 * i);
         }
 
     }
 
-    @Test // Annotatie
-    void JavaTest5(){
+    @Test
+        // Annotatie
+    void JavaTest5() {
         double cijfer1 = 0.0;
         double cijfer2 = 0.0;
         System.out.println(cijfer1 / cijfer2);
     }
 
-    @Test // Annotatie
+    @Test
+        // Annotatie
     void HalveDriehoekMetSterren() {
         for (int i = 1; i < 10; i += 2) {
             for (int j = 0; j < i; j++) {
@@ -73,23 +79,22 @@ public class JavaTestDag05 {
         }
     }
 
-    @Test // Annotatie
-    void DriehoekMetSterren(){
-        for (int i=1; i<10; i += 2)
-        {
-            for (int k=0; k < (4 - i / 2); k++)
-            {
+    @Test
+        // Annotatie
+    void DriehoekMetSterren() {
+        for (int i = 1; i < 10; i += 2) {
+            for (int k = 0; k < (4 - i / 2); k++) {
                 System.out.print(" ");
             }
-            for (int j=0; j<i; j++)
-            {
+            for (int j = 0; j < i; j++) {
                 System.out.print("'");
             }
             System.out.println("");
         }
     }
 
-    @Test // Annotatie
+    @Test
+        // Annotatie
     void OmgekeerdeHalveDriehoek() {
         for (int i = 1; i < 10; i += 2) {
             for (int j = 10; j > i; j--) {
@@ -99,15 +104,15 @@ public class JavaTestDag05 {
         }
     }
 
+    @Test
+        // Annotatie
+    void TestVanSubstrings() {
+        String tekst = "Hello world!";
+        String tekst1 = tekst.substring(6);
+        System.out.println(tekst1);
+    }
+    }
 
 
 
 
-
-
-
-
-
-
-
-}
