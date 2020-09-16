c22b9b78b6dfa3218ead9aad3c53f426d4abc726
cihangir32 <cihangirfb1@gmail.com>
2020-09-16 10:54:28 +0200

    Huiswerk opdracht deel 1&2 toegevoegd

--- src/test/java/com/example/cihangir/dag05/JavaTestDag05.java
+++ src/test/java/com/example/cihangir/dag05/JavaTestDag05.java
@@ -1,11 +1,61 @@
 package com.example.cihangir.dag05;
 
 import org.assertj.core.data.Offset;
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class JavaTestDag05 {
+
+    // Huiswerk Opdracht: Deel 1 & 2
+    @Test
+        // Annotatie
+    void DriehoekMetBasisLinks() {
+        int rij = 5;
+        for (int count= 0; count<= rij-1 ; count++)
+        {
+            for (int tel=0; tel<=count; tel++)
+            {
+                System.out.print("-"+ " ");
+            }
+            System.out.println("");
+        }
+        for (int count=rij-1; count>=0; count--)
+        {
+            for(int tel=0; tel <= count-1;tel++)
+            {
+                System.out.print("-"+ " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    @Test // Annotatie
+    void DriehoekMetBasisOnder() {
+        int rij = 5;
+        for (int count = 1; count <= rij; count++) {
+
+            for (int tel = rij; tel >= count; tel--) {
+                System.out.print(" ");
+            }
+
+            for (int tel = 1; tel <= count; tel++) {
+                System.out.print("- ");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
+
+    // Verschillende tests
     @Test
         // Annotatie
     void JavaTest() {
@@ -111,8 +161,92 @@ public class JavaTestDag05 {
         String tekst1 = tekst.substring(6);
         System.out.println(tekst1);
     }
+
+    @Test
+        // Annotatie
+    void Optellen() {
+        int[] a = {1, 1, 1, 1, 1};
+        int sum = IntStream.of(a).sum();
+        System.out.println("sum is " + sum);
     }
 
 
 
 
+// Work in progress
+        @Test
+    void Namen() {
+        // Dit is de array met strings met namen van de klas
+        String[] namen = {"Cihangir", "Sjoerd", "Majdid", "Rein", "Dave", "Evan", "Dennis", "Mike", "Joshua", "Sam", "Joris"};
+
+        // Hier print ik de eerste naam uit naar de console als een test
+        System.out.println(namen[0]);
+        // Hier print ik het totaal aantal namen uit naar de console als een test
+        System.out.println("Totaal aantal namen: " + namen.length);
+
+
+        // Hier geef ik elke naam een eigen variabele
+        String naam1 = namen[0];
+        String naam2 = namen[1];
+        String naam3 = namen[2];
+        String naam4 = namen[3];
+        String naam5 = namen[4];
+        String naam6 = namen[5];
+        String naam7 = namen[6];
+        String naam8 = namen[7];
+        String naam9 = namen[8];
+        String naam10 = namen[9];
+        String naam11 = namen[10];
+
+
+        // Variabele voor het totaal aantal woorden
+        int totaalAantalLetters = 0;
+        // Hier maak ik een for loop waarbij
+        for (int bereken = 0; bereken < namen.length; bereken++) {
+            totaalAantalLetters += namen[bereken].length();
+        }
+        // Hier print ik het totaal aantal letters uit naar de console
+        System.out.println("Totaal aantal letters: " + totaalAantalLetters);
+
+
+        // Hier geef ik de lengte van de namen een eigen nieuwe int variabele
+        int naam1int = naam1.length();
+        int naam2int = naam2.length();
+        int naam3int = naam3.length();
+        int naam4int = naam4.length();
+        int naam5int = naam5.length();
+        int naam6int = naam6.length();
+        int naam7int = naam7.length();
+        int naam8int = naam8.length();
+        int naam9int = naam9.length();
+        int naam10int = naam10.length();
+        int naam11int = naam11.length();
+
+
+        // Hierbij maak ik een nieuwe array voor de int variabele
+        int[] namenlengte = {naam1int, naam2int, naam3int, naam4int, naam5int, naam6int, naam7int, naam8int, naam9int, naam10int, naam11int};
+
+        // Hier doe ik alle variabele uit de array in een variabele om deze te printen naar de console
+        List alleArrays = Arrays.asList(namenlengte);
+        System.out.println(alleArrays);
+
+
+        int kortsteNaam = 0;
+
+        // for (int bereken = 0; bereken < namen.length; bereken++) {
+        //            totaalAantalLetters += namen[bereken].length();
+
+
+        for (int tel = 0; tel < namen.length; tel++) {
+            if (tel < 0) {
+                // block of code to be executed if the condition is true
+            } else {
+                // block of code to be executed if the condition is false
+            }
+            }
+
+
+        }
+
+
+}
